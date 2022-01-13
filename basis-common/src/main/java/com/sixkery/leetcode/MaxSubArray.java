f08a9b6ce805efeae53c2ff9f2873a30553aabf8
sixkery <sixkery@163.com>
2022-01-13 22:03:16 +0800

    最大子数和

---
+++ basis-common/src/main/java/com/sixkery/leetcode/MaxSubArray.java
@@ -0,0 +1,36 @@
+package com.sixkery.leetcode;
+
+/**
+ * 53. 最大子数组和
+ * created by sixkery on 2022/1/13.
+ */
+public class MaxSubArray {
+
+    public static int maxSubArray(int[] nums) {
+        // 当前最大值
+        int currentSum = 0;
+        // 最终结果
+        int sum = nums[0];
+        for (int num : nums) {
+            if (currentSum > 0) {
+                currentSum += num;
+            } else {
+                currentSum = num;
+            }
+            sum = Math.max(currentSum, sum);
+        }
+        return sum;
+
+
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        final int i = maxSubArray(nums);
+        System.out.println("i = " + i);
+
+    }
+
+
+}
