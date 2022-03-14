e4a37ba8e3557ecc29e774f8c8eb93dc9880990e
sixkery <sixkery@163.com>
2022-03-14 11:19:03 +0800

    newList

---
+++ basis-common/src/main/java/com/sixkery/leetcode/NewList.java
@@ -0,0 +1,25 @@
+package com.sixkery.leetcode;
+
+
+/**
+ * created by sixkery on 2022/3/10.
+ */
+public class NewList {
+
+    private Object[] elementData;
+    private int size;
+    
+    public NewList(){
+
+    }
+
+    public NewList(int init){
+        try {
+            
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        elementData = new Object[init];
+
+    }
+}
