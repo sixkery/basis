054e350ef10cf23188058b6108cb0637e61c9d1f
sixkery <sixkery@163.com>
2023-06-06 18:00:01 +0800

    Tue Jun  6 18:00:00 CST 2023

---
+++ basis-common/src/main/java/com/sixkery/leetcode/code1_10/NodeListClass.java
@@ -0,0 +1,27 @@
+package com.sixkery.leetcode.code1_10;
+
+import com.sixkery.leetcode.entity.ListNode;
+
+/**
+ * @author sixkery
+ * @since 2023/6/6
+ */
+public class NodeListClass {
+
+    public ListNode revoseNode(ListNode node) {
+         ListNode listNode = new ListNode(0);
+        ListNode head = listNode;
+
+        while (node !=null) {
+
+            listNode1.val
+            head.val.next = node.val;
+            head.val.next = node.next;
+            listNode =  node.next;
+        }
+
+        return head;
+
+
+    }
+}
