24040bf94223fe96ac1ab3915659f7f7050a268f
sixkery <sixkery@163.com>
2023-06-08 18:00:00 +0800

    Thu Jun  8 18:00:00 CST 2023

--- basis-common/src/main/java/com/sixkery/leetcode/code1_10/ReverseListNode.java
+++ basis-common/src/main/java/com/sixkery/leetcode/code1_10/ReverseListNode.java
@@ -3,7 +3,7 @@ package com.sixkery.leetcode.code1_10;
 import com.sixkery.leetcode.entity.ListNode;
 
 /**
- * 反转链表
+ * 206. 反转链表 easy
  *
  * @author sixkery
  * @since 2023/6/7
@@ -14,7 +14,9 @@ public class ReverseListNode {
             return head;
         }
         ListNode cur = reverseListNode(head.next);
+        // 把最后一位的下个指针指向前一位
         head.next.next = head;
+        // 倒数第二位的指针断掉
         head.next = null;
         return cur;
     }
