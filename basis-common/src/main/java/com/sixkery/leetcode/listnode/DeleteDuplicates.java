5b85355773145fc36eb02c11ffc4f5957de32abf
sixkery <sixkery@163.com>
2023-06-12 18:00:00 +0800

    Mon Jun 12 18:00:00 CST 2023

---
+++ basis-common/src/main/java/com/sixkery/leetcode/listnode/DeleteDuplicates.java
@@ -0,0 +1,28 @@
+package com.sixkery.leetcode.listnode;
+
+import com.sixkery.leetcode.entity.ListNode;
+
+/**
+ * 83. 删除排序链表中的重复元素
+ * 输入：head = [1,1,2]
+ * 输出：[1,2]
+ *
+ * @author sixkery
+ * @since 2023/6/12
+ */
+public class DeleteDuplicates {
+    public ListNode deleteDuplicates(ListNode head) {
+
+        ListNode cur = head;
+        while (cur != null && cur.next != null) {
+            if (cur.val == cur.next.val) {
+                cur.next = cur.next.next;
+            } else {
+                cur = cur.next;
+            }
+        }
+        return head;
+
+
+    }
+}
