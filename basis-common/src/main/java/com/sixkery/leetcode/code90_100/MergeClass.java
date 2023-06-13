1fb3903277a9d7d14cf8451ce1f57640b6fc1c8b
sixkery <sixkery@163.com>
2023-06-13 18:00:00 +0800

    Tue Jun 13 18:00:00 CST 2023

---
+++ basis-common/src/main/java/com/sixkery/leetcode/code90_100/MergeClass.java
@@ -0,0 +1,34 @@
+package com.sixkery.leetcode.code90_100;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author sixkery
+ * @since 2023/6/13
+ */
+public class MergeClass {
+
+
+    public List<Integer> get(List<Integer> ids) {
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+    public List<Integer> getData(List<Integer> ids) {
+
+
+        return
+    }
+}
