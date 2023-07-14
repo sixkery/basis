10f3bd4050ecd798c5765d9e77d0aa48f015201b
sixkery <sixkery@163.com>
2023-07-14 18:00:01 +0800

    Fri Jul 14 18:00:01 CST 2023

--- basis-common/src/main/java/com/sixkery/leetcode/array/FindKthLargest.java
+++ basis-common/src/main/java/com/sixkery/leetcode/array/FindKthLargest.java
@@ -7,25 +7,30 @@ import java.util.Arrays;
  * 给定整数数组 nums 和整数 k，请返回数组中第 k 个最大的元素。
  * 请注意，你需要找的是数组排序后的第 k 个最大的元素，而不是第 k 个不同的元素。
  * 你必须设计并实现时间复杂度为 O(n) 的算法解决此问题。
- *
+ * <p>
  * 输入: [3,2,1,5,6,4], k = 2
  * 输出: 5
+ *
  * @author sixkery
  * @since 2023/7/10
  */
 public class FindKthLargest {
 
-    public int findKthLargest(int[] nums, int k) {
+    public static int findKthLargest(int[] nums, int k) {
 
+        Arrays.sort(nums);
 
-       Arrays.sort(nums);
+        int length = nums.length;
+        while (k != 0) {
+            k--;
+            length--;
+        }
+        return nums[length];
 
-       int length = nums.length - 1;
-       while (k!=0) {
-           k--;
-           length--;
-       }
-       return nums[length + 1];
+    }
+
+    public static void main(String[] args) {
+
 
     }
 }
