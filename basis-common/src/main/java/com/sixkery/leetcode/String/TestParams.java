1423cf886180688303db8efb4da518b5f78febb2
sixkery <sixkery@163.com>
2023-08-16 18:00:01 +0800

    Wed Aug 16 18:00:01 CST 2023

--- basis-common/src/main/java/com/sixkery/leetcode/String/TestParams.java
+++ basis-common/src/main/java/com/sixkery/leetcode/String/TestParams.java
@@ -1,15 +1,44 @@
 package com.sixkery.leetcode.String;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+
 /**
  * @author sixkery
  * @since 2023/8/14
  */
 public class TestParams {
     public static void main(String[] args) {
-        int n = 5;
-        System.out.println("n 的初始值为：" + n);
-        add(n);
-        System.out.println("n 的最终值为：" + n);
+//        int n = 5;
+//        System.out.println("n 的初始值为：" + n);
+//        add(n);
+//        System.out.println("n 的最终值为：" + n);
+
+         ArrayList<Integer> list = new ArrayList<>();
+
+        list.add(1);
+        list.add(2);
+
+         HashSet<Integer> objects = new HashSet<>();
+
+        objects.add(1);
+        objects.add(2);
+        objects.add(3);
+
+        HashSet<Integer> listset = new HashSet<>(list);
+
+        for (Integer object : objects) {
+            if (listset.add(object)){
+                list.add(object);
+            }
+        }
+
+
+        System.out.println("listset = " + list);
+
+
+
+
     }
     public static void add(int n) {
         n = n + 1;
