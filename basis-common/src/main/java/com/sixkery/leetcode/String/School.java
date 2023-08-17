505a9b8ebb41af8a6aff2d5b7370a528e511a3db
sixkery <sixkery@163.com>
2023-08-17 18:00:00 +0800

    Thu Aug 17 18:00:00 CST 2023

---
+++ basis-common/src/main/java/com/sixkery/leetcode/String/School.java
@@ -0,0 +1,51 @@
+package com.sixkery.leetcode.String;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author sixkery
+ * @since 2023/8/17
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class School {
+
+    public int id;
+
+    public String name;
+
+    public String age;
+
+}
+
+@Data
+@EqualsAndHashCode(callSuper = true)
+class ShangHai extends School {
+    private String type;
+}
+
+
+class test {
+
+    public static void main(String[] args) {
+        ShangHai shangHai = new ShangHai();
+        shangHai.setId(1);
+        shangHai.setName("上海大学");
+        shangHai.setType("A");
+
+        ShangHai shangHai2 = new ShangHai();
+        shangHai2.setId(2);
+        shangHai2.setName("华东师范");
+        shangHai2.setType("A");
+
+        System.out.println(shangHai.equals(shangHai2));
+        System.out.println(shangHai.toString());
+        System.out.println(shangHai2.toString());
+
+    }
+}
+
